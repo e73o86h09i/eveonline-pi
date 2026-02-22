21e858f49d4f7b837ffcd90c54c28a7e8aff7c5c
Denis Pakush <e73o86h09i@gmail.com>
2026-02-22 11:25:24 +0100

    feat: add favicon and logo assets, update header to include logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import type { FC } from 'react';
 import { Navbar, NavbarBrand } from 'flowbite-react';
+import logo from '../../assets/EveOnline-PI-Calculator-Logo.png';
 
 const Header: FC = () => (
   <Navbar fluid className="border-b border-gray-700 bg-gray-800">
     <NavbarBrand href="/">
+      <img src={logo} alt="EVE Online PI Calculator" className="mr-3 h-8" />
       <span className="self-center whitespace-nowrap text-xl font-semibold text-white">EVE Online PI Calculator</span>
     </NavbarBrand>
   </Navbar>
